8227a1fe7d01ae4be29aa9ea9ae20ddeb497a13b
ksy <tjssud0647@gmail.com>
2026-02-12 15:35:13 +0900

    [chore] 불필요한 Flyway 체크 코드와 오타 어노테이션 제거
    - Flyway 미사용 구조에 맞춰 BackendApplication의 디버그용 CommandLineRunner를 제거
    
    - 사용되지 않는 RequesMapping 어노테이션 파일을 삭제해 코드 노이즈를 줄임

--- src/main/java/com/apiguard/backend/BackendApplication.java
+++ src/main/java/com/apiguard/backend/BackendApplication.java
@@ -2,10 +2,6 @@ package com.apiguard.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // JPA Auditing 활성화
@@ -14,15 +10,5 @@ public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner flywayCheck(ApplicationContext ctx) {
-        return args -> {
-            System.out.println(">>> Flyway Bean Exists? " + ctx.containsBean("flyway"));
-            if (ctx.containsBean("flyway")) {
-                System.out.println(">>> Flyway Bean Class: " + ctx.getBean("flyway").getClass().getName());
-            }
-        };
     }
 }
